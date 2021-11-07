1eb996a84d9734f7a0d1f6003587d207835d0401
Anda <muntean.anda2000@gmail.com>
2021-11-07 20:40:59 +0200

    implement ICBroker interface and extended it in ICBrokerImpl

--- StockMarketSimulator/src/clientbroker/ICBroker.java
+++ StockMarketSimulator/src/clientbroker/ICBroker.java
@@ -1,5 +1,6 @@
 package clientbroker;
 
+import java.util.List;
 import java.util.function.BiConsumer;
 import java.util.function.BiFunction;
 
@@ -8,6 +9,13 @@ import client.dtos.StockTransaction;
 
 public interface ICBroker {
 
-	void addOffer(String stockID, StockOffer offer, BiConsumer<Boolean,StockTransaction> callback);
-	
+	public void addOffer(String stockID, StockOffer offer, BiConsumer<Boolean,StockTransaction> callback);
+    public double getStockPrice(String stockID);
+    public List<String> getStockList();
+    public List<StockOffer> getStockBuyOffers(String stockID);
+    public List<StockOffer>  getStockSellOffers(String stockID);
+    public List<StockTransaction> getStockHistory(String stockID);
+    public String modifyOffer(String offerID, StockOffer newOffer);
+    public int offerCallback(boolean trSucc, StockTransaction transaction);
+
 }
--- StockMarketSimulator/src/clientbroker/ICBrokerImpl.java
+++ StockMarketSimulator/src/clientbroker/ICBrokerImpl.java
@@ -1,5 +1,6 @@
 package clientbroker;
 
+import java.util.List;
 import java.util.function.BiConsumer;
 
 import client.dtos.StockOffer;
@@ -14,4 +15,47 @@ public class ICBrokerImpl implements ICBroker{
 		
 	}
 	
+
+    @Override
+    public double getStockPrice(String stockID) {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public List<String> getStockList() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public List<StockOffer> getStockBuyOffers(String stockID) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public List<StockOffer> getStockSellOffers(String stockID) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public List<StockTransaction> getStockHistory(String stockID) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public String modifyOffer(String offerID, StockOffer newOffer) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public int offerCallback(boolean trSucc, StockTransaction transaction) {
+        // TODO Auto-generated method stub
+        return 0;
 }
+}
+
