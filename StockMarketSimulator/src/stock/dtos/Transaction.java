3f1424e6ef423a584c93de6230d3a188f46831b9
Cosmina <sas.cosmina99@gmail.com>
2021-11-07 12:05:03 +0200

    CEBP-6: Create Transaction dto

--- StockMarketSimulator/src/stock/dtos/Transaction.java
+++ StockMarketSimulator/src/stock/dtos/Transaction.java
@@ -1,27 +1,71 @@
 package stock.dtos;
 
-public class Transaction {
-	private int ID;
+import java.time.LocalDateTime;
 
-	/**
-	 * @param iD
-	 */
-	public Transaction(int iD) {
+public class Transaction {
+
+	private String ID;
+	private Offer sellOffer;
+	private Offer buyOffer;
+	private String stockID;
+	private int test;
+	private LocalDateTime timestamp;
+
+	public Transaction(String iD, Offer sellOffer, Offer buyOffer, String stockID, int test, LocalDateTime timestamp) {
 		ID = iD;
+		this.sellOffer = sellOffer;
+		this.buyOffer = buyOffer;
+		this.stockID = stockID;
+		this.test = test;
+		this.timestamp = timestamp;
 	}
 
-	/**
-	 * @return the iD
-	 */
-	public int getID() {
+	public String getID() {
 		return ID;
 	}
 
-	/**
-	 * @param iD the iD to set
-	 */
-	public void setID(int iD) {
+	public void setID(String iD) {
 		ID = iD;
 	}
-	
+
+	public Offer getSellOffer() {
+		return sellOffer;
+	}
+
+	public void setSellOffer(Offer sellOffer) {
+		this.sellOffer = sellOffer;
+	}
+
+	public Offer getBuyOffer() {
+		return buyOffer;
+	}
+
+	public void setBuyOffer(Offer buyOffer) {
+		this.buyOffer = buyOffer;
+	}
+
+	public String getStockID() {
+		return stockID;
+	}
+
+	public void setStockID(String stockID) {
+		this.stockID = stockID;
+	}
+
+	public int getTest() {
+		return test;
+	}
+
+	public void setTest(int test) {
+		this.test = test;
+	}
+
+	public LocalDateTime getTimestamp() {
+		return timestamp;
+	}
+
+	public void setTimestamp(LocalDateTime timestamp) {
+		this.timestamp = timestamp;
+	}
+
 }
