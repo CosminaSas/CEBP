40d353015a976f39ec5f05df02e2dec05b19d655
Anda <muntean.anda2000@gmail.com>
2021-11-07 23:02:33 +0200

    not functional-some tryings

--- StockMarketSimulator/src/clientbroker/ICBrokerImpl.java
+++ StockMarketSimulator/src/clientbroker/ICBrokerImpl.java
@@ -1,21 +1,31 @@
 package clientbroker;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.function.BiConsumer;
 
 import client.dtos.StockOffer;
 import client.dtos.StockTransaction;
+import stock.Stock;
 
 public class ICBrokerImpl implements ICBroker{
+
+	private volatile boolean running;
+	private Map<String, Stock> stocks = new ConcurrentHashMap<String, Stock>(); //client-stock, broker=stocks?
+	private List<StockTransaction> transactions = new ArrayList<>();
 
 	@Override
 	public void addOffer(String stockID, StockOffer offer, BiConsumer<Boolean, StockTransaction> callback) {
 		
-		callback.accept(false, null);
+		Map<String, Stock> broker;
+		Stock so = broker.get(stockID);
+		so.addOffer(offer);
+		//callback.accept(false, null);
 		
 	}
 	
-
     @Override
     public double getStockPrice(String stockID) {
         // TODO Auto-generated method stub
@@ -30,7 +40,10 @@ public class ICBrokerImpl implements ICBroker{
 
     @Override
     public List<StockOffer> getStockBuyOffers(String stockID) {
-        // TODO Auto-generated method stub
+        // TODO Auto-generated method stub //return lista de offer, pt fiecare offer fac un for pt, return lista de stock offers, 
+		//ma uit ce apping am in offers, pot sa le iau pe toate su ke oun in stockOffer
+		List<Offer> off = broker.getStockSellOffers(stockID);
+		
         return null;
     }
 
