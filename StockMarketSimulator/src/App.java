566d41f10a9e035ce2b64a4d9db83b9091058321
Szasz Tamas <tqmi.szasz@gmail.com>
2021-11-12 23:00:47 +0200

    Basic test for testing some functionality

--- StockMarketSimulator/src/App.java
+++ StockMarketSimulator/src/App.java
@@ -2,16 +2,52 @@ import java.util.concurrent.ThreadLocalRandom;
 
 import broker.IBroker;
 import broker.IBrokerImpl;
+import client.Client;
+import clientbroker.ICBrokerImpl;
+import common.OfferType;
+import stock.Stock;
 import stock.dtos.Transaction;
 
 public class App {
     public static void main(String[] args) throws Exception {
+
+
         IBroker broker = new IBrokerImpl();
-        Thread t = new Thread(broker,"Broker");
-        t.start();
-        Thread.sleep(10000);
-        ((IBrokerImpl)broker).setRunning(false);
-        t.join();
+        Client c1 = new Client("id 1", new ICBrokerImpl(broker,"id 1"));
+        Client c2 = new Client("id 1", new ICBrokerImpl(broker,"id 2"));
+
+        Stock s1 = new Stock("s1");
+        Stock s2 = new Stock("s2");
+        Stock s3 = new Stock("s3");
+        Stock s4 = new Stock("s4");
+        Stock s5 = new Stock("s5");
+
+        c1.getStocks();
+        broker.subscribe(s1);
+        c1.getStocks();
+        broker.subscribe(s2);
+        c1.getStocks();
+        broker.subscribe(s3);
+        c1.getStocks();
+        broker.subscribe(s4);
+        c1.getStocks();
+        broker.subscribe(s5);
+        c1.getStocks();
+        
+        c1.getBuyOffers(s1.getID());
+        c1.addOffer(s1.getID(), 2, 3.5, OfferType.BUY);
+        c1.getBuyOffers(s1.getID());
+
+        c1.addOffer(s1.getID(), 3, 3.5, OfferType.BUY);
+        c1.addOffer(s1.getID(), 4, 3.5, OfferType.BUY);
+        c1.addOffer(s1.getID(), 5, 3.5, OfferType.BUY);
+        c1.addOffer(s1.getID(), 6, 3.5, OfferType.BUY);
+        c1.getBuyOffers(s1.getID());
+
+        c2.getBuyOffers(s1.getID());
+
+        c1.addOffer(s1.getID(), 6, 3.5, OfferType.SELL);
+        c1.getSellOffers(s1.getID());
     }
 
     
