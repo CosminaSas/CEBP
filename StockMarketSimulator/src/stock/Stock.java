824024c852c8e4947450e51051e4c15bbfda391b
Cosmina <sas.cosmina99@gmail.com>
2021-11-19 20:52:09 +0200

    CEBP-15: Improve Stock class

--- StockMarketSimulator/src/stock/Stock.java
+++ StockMarketSimulator/src/stock/Stock.java
@@ -3,6 +3,7 @@ package stock;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.BiConsumer;
 
 import broker.IBroker;
 import common.OfferType;
@@ -16,8 +17,8 @@ public class Stock {
 	private List<Offer> sellOffers = new ArrayList<>();
 	private List<Transaction> transactionHistory = new ArrayList<>();
 	private IBroker broker;
-
-	
+	private Offer minSell;
+	private Offer maxBuy;
 
 	/**
 	 * @param iD
@@ -99,26 +100,27 @@ public class Stock {
 	}
 
 	public double getPrice() {
-		double min_price = 0;
-
-		for (Offer offer : sellOffers) {
-			min_price = Math.min(min_price, offer.getPrice());
-		}
-
-		return min_price;
+		return minSell.getPrice();
 	}
 
 	public boolean addOffer(Offer offer) {
 
 		System.out.println("stock " + ID + "adding offer " + offer);
 
-
 		if (offer.getOfferType() == OfferType.BUY) {
 			buyOffers.add(offer);
+			System.out.println("offer added: " + offer.getID());
+			if (maxBuy.getPrice() < offer.getPrice())
+				maxBuy = offer;
+			this.notify();
 			return true;
 
 		} else if (offer.getOfferType() == OfferType.SELL) {
 			sellOffers.add(offer);
+			System.out.println("offer added: " + offer.getID());
+			if (minSell.getPrice() > offer.getPrice())
+				minSell = offer;
+			this.notify();
 			return true;
 		}
 
@@ -129,8 +131,12 @@ public class Stock {
 		if (newOffer.getOfferType() == OfferType.BUY) {
 			for (Offer offer : buyOffers) {
 				if (offer.getID() == offerID) {
+					if (maxBuy.getID() == offerID) {
+						maxBuy = getMax();
+					}
 					buyOffers.remove(offer);
 					buyOffers.add(newOffer);
+					this.notify();
 					return 1;
 				}
 			}
@@ -138,8 +144,12 @@ public class Stock {
 		} else if (newOffer.getOfferType() == OfferType.SELL) {
 			for (Offer offer : sellOffers) {
 				if (offer.getID() == offerID) {
+					if (minSell.getID() == offerID) {
+						minSell = getMin();
+					}
 					sellOffers.remove(offer);
 					sellOffers.add(newOffer);
+					this.notify();
 					return 1;
 				}
 			}
@@ -148,9 +158,42 @@ public class Stock {
 		return 0;
 	}
 
+	// cu for
+	private Offer getMin() {
+		double min_price = sellOffers.get(0).getPrice();
+		Offer min_offer = sellOffers.get(0);
+
+		for (Offer offer : sellOffers) {
+			if (min_price > offer.getPrice())
+				min_offer = offer;
+		}
+
+		return min_offer;
+	}
+
+	private Offer getMax() {
+		double max_price = sellOffers.get(0).getPrice();
+		Offer max_offer = sellOffers.get(0);
+
+		for (Offer offer : sellOffers) {
+			if (max_price < offer.getPrice())
+				max_offer = offer;
+		}
+
+		return max_offer;
+	}
+
 	public void run() {
 		while (running) {
 			cyclic();
+			synchronized (this) {
+				try {
+					this.wait(1000);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
 		}
 	}
 
@@ -189,4 +232,5 @@ public class Stock {
 
 		return sb.toString();
 	}
+
 }
