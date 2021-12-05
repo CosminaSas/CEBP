4dafb80715bd44f85216fb5ad48d7be11742002a
Szasz Tamas <tqmi.szasz@gmail.com>
2021-12-05 16:44:29 +0200

    Offer class now immutable

--- StockMarketSimulator/src/stock/dtos/Offer.java
+++ StockMarketSimulator/src/stock/dtos/Offer.java
@@ -4,18 +4,18 @@ import java.util.function.BiConsumer;
 
 import common.OfferType;
 
-public class Offer implements Comparable<Offer>{
+public final class Offer implements Comparable<Offer>{
 
     private static int IDs = 0;
 
-    private String ID; // sau string??
-    private String clientID; // sau string??
-    private String stockID;
-    private double price;
-    private int quantity;
-    private OfferType offerType;
-    private BiConsumer<Boolean, Transaction> callback;
-    private long createdAt;
+    private final String ID; // sau string??
+    private final String clientID; // sau string??
+    private final String stockID;
+    private final double price;
+    private final int quantity;
+    private final OfferType offerType;
+    private final BiConsumer<Boolean, Transaction> callback;
+    private final long createdAt;
 
     public Offer(String clientID, String stockID,double price, int quantity, OfferType offerType,BiConsumer<Boolean, Transaction> callback) {
         this.createdAt = System.currentTimeMillis();
@@ -29,12 +29,6 @@ public class Offer implements Comparable<Offer>{
 
     }
     
-	/**
-     * @param stockID the stockID to set
-     */
-    public void setStockID(String stockID) {
-        this.stockID = stockID;
-    }
 
     /**
      * @return the callback
@@ -43,51 +37,26 @@ public class Offer implements Comparable<Offer>{
         return callback;
     }
 
-    /**
-     * @param callback the callback to set
-     */
-    public void setCallback(BiConsumer<Boolean, Transaction> callback) {
-        this.callback = callback;
-    }
-
     public String getID() {
         return ID;
-    }
-
-    public void setID(String iD) {
-        ID = iD;
     }
 
     public String getClientID() {
         return clientID;
     }
 
-    public void setClientID(String clientID) {
-        this.clientID = clientID;
-    }
 
     public double getPrice() {
         return price;
     }
 
-    public void setPrice(double price) {
-        this.price = price;
-    }
 
     public int getQuantity() {
         return quantity;
     }
 
-    public void setQuantity(int quantity) {
-        this.quantity = quantity;
-    }
-
     public OfferType getOfferType() {
         return offerType;
-    }
-
-    public void setOfferType(OfferType offerType) {
-        this.offerType = offerType;
     }
 
 	public String getStockID() {
